eeae4488bae2226007882204ac7dffba0028315c
Vukasin <vukasinzivaljevic13@gmail.com>
2026-03-25 22:07:49 +0100

    napravljen projekat

---
+++ ZelezniceSrbije.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ZelezniceSrbije.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ZelezniceSrbije/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ZelezniceSrbije.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
